547b58ca41f4f5871d5c33c44d46038b75f4aaac
leelavathikm1 <leelavathikm1@gmail.com>
2022-01-03 21:36:03 +0530

    Day 1 Code

---
+++ Intro/main.js
@@ -0,0 +1,8 @@
+// alert("Hello Javscript")
+//    var answer =  confirm("hello .. Are you interested to learn Javascrip ?")
+//    alert(answer)
+
+var userName = prompt("Please enter your Name");
+var newPerson = "Murali"
+// var new ="some"
+alert(userName);

c3a24253146d627b6cbcc360b6ebd0ded98d65d4
SovanRoy-coder <sovanndxalpha15@gmail.com>
2022-01-05 01:36:02 +0530

    commit

---
+++ DAY1 CODE/main.js
@@ -0,0 +1,8 @@
+// alert("Hello Javscript")
+//    var answer =  confirm("hello .. Are you interested to learn Javascript ?")
+//    alert(answer)
+
+var userName = prompt("Please enter your Name");
+var newPerson = "Sovan";
+// var new ="some"
+alert(userName);

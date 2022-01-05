4e1b807c98449817fd78e1887fc45d3af5faf7e1
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-05 23:04:24 +0530

    Use res.json() to send array as response to GET request

--- 05Jan2022/NodeJS/express_server/index.js
+++ 05Jan2022/NodeJS/express_server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let users = ["Prateek", "Joshi", "Takur", "Ravi Kiran"];
 app.get("/", (req, res) => {
     // res.json({msg:'Hello From Default Route'})
     // res.send("Hello from Express")
+    res.json({ users });
     // res.write();
-    res.send("Hello from Write...")
+    // res.send("Hello from Write...")
 });
 
 app.listen(port, () => {

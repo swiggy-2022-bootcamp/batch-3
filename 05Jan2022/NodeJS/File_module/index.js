72128c61a51913bdc59e8f3fa37b9b46f42138a9
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-05 22:41:39 +0530

    Use fs.writeFile() to create a file

--- 05Jan2022/NodeJS/File_module/index.js
+++ 05Jan2022/NodeJS/File_module/index.js
@@ -4,6 +4,10 @@ var fs = require("fs");
 //     console.log(data);
 // });
 
-fs.readFile('./Book1.xlsx', 'utf-8', (err, data) => {
-    console.log(data)
+// fs.readFile('./Book1.xlsx', 'utf-8', (err, data) => {
+//     console.log(data)
+// })
+
+fs.writeFile('./HelloSome.txt', "Hello written from Node JS", (err, data) => {
+    console.log("Hello File Created")
 })

30acd01a51fb9ef3c1c3eb462ec9fee73f7d0a79
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-01-05 18:28:06 +0530

    For in Loop

---
+++ JavaScript/Iteratives/main.js
@@ -0,0 +1,14 @@
+// Loop Statements
+
+var person = {
+  fname: "Virang", // property --> Key-value pair
+  lname: "Parekh",
+  email: "virangparekh73@gmail.com",
+  mobile: 83908390892,
+};
+
+//for in loop
+
+for (key in person) {
+  console.log(person[key]);
+}

a1161ffc9cbe3e8d925bd166b7f31ac85382493a
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-01-05 10:55:51 +0530

    Alerts and Prompts

---
+++ JavaScript/Alerts/main.js
@@ -0,0 +1,5 @@
+// Alerts, Prompts and Displaying
+
+var username = prompt("Hello What's your name?"); // prompt is used to input
+var newPerson = "Hi There "; //declaring new string
+alert(newPerson + username);

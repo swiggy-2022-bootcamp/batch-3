6e741d5715390845a716eb634966bc91feb0ae7e
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-05 22:39:43 +0530

    Use fs.readFile() to read .xlsx file

--- 05Jan2022/NodeJS/File_module/index.js
+++ 05Jan2022/NodeJS/File_module/index.js
@@ -1,5 +1,9 @@
 var fs = require("fs");
 
-fs.readFile("./hello.txt", "utf-8", (err, data) => {
-    console.log(data);
-});
+// fs.readFile("./hello.txt", "utf-8", (err, data) => {
+//     console.log(data);
+// });
+
+fs.readFile('./Book1.xlsx', 'utf-8', (err, data) => {
+    console.log(data)
+})

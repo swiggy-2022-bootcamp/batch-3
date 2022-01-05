99712f919e7dba47132d67dfef947ea7949a403c
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-01-05 18:02:46 +0530

    Alerts and Popup boxes

--- JavaScript/Alerts/main.js
+++ JavaScript/Alerts/main.js
@@ -3,3 +3,9 @@
 var username = prompt("Hello What's your name?"); // prompt is used to input
 var newPerson = "Hi There "; //declaring new string
 alert(newPerson + username);
+var opinion = confirm("Do you like waking up early in the morning?");
+if (opinion) {
+  alert("You are an early bird!");
+} else {
+  alert("You are a night owl!");
+}

853839b7c0df80dc16cf26b5ecc48e37db36c74b
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-01-05 18:22:09 +0530

    Updated js to OOP objects

--- JavaScript/Objects/main.js
+++ JavaScript/Objects/main.js
@@ -22,3 +22,42 @@ function handleClick() {
   document.querySelector("div").innerHTML =
     "<p>Hi there" + fname + " " + lname + "</p>";
 }
+
+//To delete a property
+
+delete person.mobile;
+
+var student = new Object({
+  fname: "Manish",
+  roll_no: 37,
+  college: "DJSCE",
+});
+
+console.log(student);
+
+function newStudent(fname, roll_no, college) {
+  this.fname = fname;
+  this.roll_no = roll_no;
+  this.college = college;
+}
+
+var std = new newStudent("Manish", 37, "DJSCE");
+console.log(std);
+
+// console.log(fname);
+
+function Employee(fname, lname, email) {
+  this.fname = fname;
+  this.lname = lname;
+  this.email = email;
+  this.city = "Bangalore";
+  this.state = "karnataka";
+  this.zip = 560066;
+  this.company = "Swiggy";
+}
+
+var std1 = new Employee("Murali", "krishna", "murali@gmail.com");
+var std2 = new Employee("Paras", "KK", "parasKK@gmail.com");
+
+console.log(std1);
+console.log(std2);

edfd84c8190afbbaf817026bc7918e58b3b65349
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-05 23:07:52 +0530

    Create POST /addUser endpoint

--- 05Jan2022/NodeJS/express_server/index.js
+++ 05Jan2022/NodeJS/express_server/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var bodyParser = require("body-parser");
 
 let users = ["Prateek", "Joshi", "Takur", "Ravi Kiran"];
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
     // res.json({msg:'Hello From Default Route'})
     // res.send("Hello from Express")
     res.json({ users });
     // res.write();
     // res.send("Hello from Write...")
+});
+
+app.post("/addUser", (req, res) => {
+    console.log(req.body);
+    users.push(req.body.name);
+    res.send({ users });
 });
 
 app.listen(port, () => {

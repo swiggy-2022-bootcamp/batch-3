a8a1191f189a73f99536b46e2f711455a7d34661
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-06 23:14:35 +0530

    Complete GET /products endpoint
    Use Promise, async-await, and try-catch concepts

--- 06Jan2022/NodeJS/express_gen_project/routes/products.js
+++ 06Jan2022/NodeJS/express_gen_project/routes/products.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const handleRequest = require("./handleRequest");
+
 var router = express.Router();
 
 /* GET products listing. */
-router.get('/', function (req, res, next) {
-    res.send('Response from  productsRouter');
+router.get("/", async function (req, res, next) {
+    try {
+        let products = await handleRequest();
+        res.send({ products: JSON.parse(products) });
+    } catch (error) {
+        res.send({ msg: "Something went wrong!!!", products: [] });
+    }
 });
 
 module.exports = router;

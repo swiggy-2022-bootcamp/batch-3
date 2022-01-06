4bc11464089ce1ad871361193fa1a507a2ddf81f
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-06 22:43:21 +0530

    Create POST /login route

--- 06Jan2022/NodeJS/express_gen_project/routes/users.js
+++ 06Jan2022/NodeJS/express_gen_project/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post("/login", (req, res) => {
+  res.send("Message from Login Route");
 });
 
 module.exports = router;

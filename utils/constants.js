8bb123479acd9b131e3f288854aef56298ba2793
sachin <sachinsom507@gmail.com>
2022-01-06 22:40:27 +0530

    Updated Constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,7 @@
 module.exports = {
     USER_REGISTERATION_SUCCESS: 'User Registration Successfully',
     USER_LOGIN_SUCCESS: 'User Login Successfully',
-    USER_LOGIN_FAILED: 'Invalid Credentials'
+    USER_LOGIN_FAILED: 'Sorry Invalid Credentials',
+    USER_AUTHORIZATION_FAILED: 'Sorry Invalid User Details',
+    QUESTION_POSTED_SUCCESS: 'Question Posted Successfully',
 }

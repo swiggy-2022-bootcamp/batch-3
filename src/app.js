c768e49e742108e190e1d8a3768f73db9c5ad6d3
Debdyut Hajra <debdyuthajra@ymail.com>
2022-01-06 21:38:00 +0530

    feat: Added file logging

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+// Swagger dependency
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('../contract/QA-Platform-API-v0.0.1.json')
 
+// Logging dependency
+const morgan = require('morgan')
+const rfs = require('rotating-file-stream')
+
+// Import controllers
 const identityRoutes = require('./routes/identity');
 const qaPlatformRoutes = require('./routes/qa-platform');
 const fallbackController = require('./controllers/fallback');
 
+// Import dao
 const QuestionsDao = require('./dao/questions-dao');
 const UsersDao = require('./dao/users-dao');
 
 const app = express()
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: 'log'
+  })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 // Enable parsing of json request body
 app.use(bodyParser.json());

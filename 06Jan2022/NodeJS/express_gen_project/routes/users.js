e23cd297cdd82860c25583a3210330816c64ff77
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-01-06 22:48:41 +0530

    Add checkUserInfo() middleware
    middleware will be called once API call to route is made, it will perform all actions specified and then only the call to route will proceed. Here checkUserInfo() verifies if the enter credentials are for Prateek(hard-coded values) or not

--- 06Jan2022/NodeJS/express_gen_project/routes/users.js
+++ 06Jan2022/NodeJS/express_gen_project/routes/users.js
@@ -6,12 +6,26 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post("/login", (req, res) => {
-  res.send("Message from Login Route");
+router.post("/login", checkUserInfo, (req, res) => {
+  res.send("Hello user .. WELCOME");
 });
 
 router.post("/register", (req, res) => {
   res.send("Message from Register Route");
 });
+
+function checkUserInfo(req, res) {
+  console.log(req.body)
+  var person = {
+    userName: "Prateek",
+    password: "Prateek",
+  };
+  if (person.userName == req.body.userName && person.password == req.body.password) {
+    next()
+  } else {
+    res.send({ msg: "User is Invalid .. Please check the Credentials" })
+  }
+  console.log("Hello from Check User Info");
+}
 
 module.exports = router;

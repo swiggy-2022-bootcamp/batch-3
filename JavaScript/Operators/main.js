cd2136438ccc83b7c208fdc49fa1117e6219bdfe
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-01-07 10:31:34 +0530

    Operators in Javascript

---
+++ JavaScript/Operators/main.js
@@ -0,0 +1,40 @@
+//Arithmetic operators
+
+var a = 10;
+var b = 11;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//comparison operators
+
+var c = "10";
+
+console.log(a == c);
+console.log(a === c);
+console.log(a !== c);
+console.log(a !== b);
+console.log(a != b);
+console.log(a >= b);
+console.log(c <= b);
+
+var d = true;
+var e = false;
+
+console.log(d || e);
+console.log(d && e);
+// console.log(object);
+console.log(!e);
+
+//ternary operator
+
+var age = 18;
+console.log(age < 18 ? "Not Eligible" : "Voting is our duty");
+
+//assignment operator
+
+var personName = "Manoj";
+var fname = personName;

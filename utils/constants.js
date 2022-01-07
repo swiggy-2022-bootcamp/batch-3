d7e87623e4271d716b9197794678888a3367069c
sachin <sachinsom507@gmail.com>
2022-01-07 11:53:33 +0530

    Added Constants

--- utils/constants.js
+++ utils/constants.js
@@ -4,4 +4,7 @@ module.exports = {
     USER_LOGIN_FAILED: 'Sorry Invalid Credentials',
     USER_AUTHORIZATION_FAILED: 'Sorry Invalid User Details',
     QUESTION_POSTED_SUCCESS: 'Question Posted Successfully',
+    QUESTION_POSTED_FAILED: 'Question Not Posted',
+    ANSWER_POSTED_SUCCESS: 'Answer Posted Succesfully',
+    ANSWER_POSTED_FAILED: 'Answer Not Posted',
 }

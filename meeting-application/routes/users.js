ea886eeefdbdc19823e43f433cff265847ee2ba1
aky91 <aakash28399@gmail.com>
2022-01-08 03:13:08 +0530

    Added DELETE route to drop off meeting

--- meeting-application/routes/users.js
+++ meeting-application/routes/users.js
@@ -20,46 +20,60 @@ router.post('/', (req, res, next) => {
   })
 });
 
+var meetings = [
+  {
+    "meeting-id" : "abc-devg-hij",
+    "date-of-meeting" : "2020-01-20",
+    "start-time" : "14:00",
+    "end-time" : "14:30",
+    "description" : "Sprint Retrospective",
+    "email-ids-of-attendees" : "irov@swiggy.com,jolly@swiggy.com,kalashnikov@swiggy.com,lango@swiggy.com"
+  },{
+    "meeting-id" : "bca-dnsg-shg",
+    "date-of-meeting" : "2020-01-07",
+    "start-time" : "11:00",
+    "end-time" : "12:00",
+    "description" : "Daily Scrum Meeting",
+    "email-ids-of-attendees" : "eric@swiggy.com,faizal@swiggy.com,greta@swiggy.com,henry@swiggy.com"
+  },{
+    "meeting-id" : "gem-asdg-asl",
+    "date-of-meeting" : "2020-01-20",
+    "start-time" : "14:00",
+    "end-time" : "14:30",
+    "description" : "Sprint Retrospective",
+    "email-ids-of-attendees" : "irov@swiggy.com,jolly@swiggy.com,kalashnikov@swiggy.com,lagno@swiggy.com"
+  }
+]
+
 /* GET all meetings for a user, validate user*/
 router.get('/meetings', (req, res, next) => {
   const {userid, password} = req.body
   const meetingId = req.body["meeting-id"]
-
-  var meetings = [
-    {
-      "meeting-id" : "abc-devg-hij",
-      "date-of-meeting" : "2020-01-20",
-      "start-time" : "14:00",
-      "end-time" : "14:30",
-      "description" : "Sprint Retrospective",
-      "email-ids-of-attendees" : "irov@swiggy.com,jolly@swiggy.com,kalashnikov@swiggy.com,lango@swiggy.com"
-    },{
-      "meeting-id" : "bca-dnsg-shg",
-      "date-of-meeting" : "2020-01-07",
-      "start-time" : "11:00",
-      "end-time" : "12:00",
-      "description" : "Daily Scrum Meeting",
-      "email-ids-of-attendees" : "eric@swiggy.com,faizal@swiggy.com,greta@swiggy.com,henry@swiggy.com"
-    },{
-      "meeting-id" : "gem-asdg-asl",
-      "date-of-meeting" : "2020-01-20",
-      "start-time" : "14:00",
-      "end-time" : "14:30",
-      "description" : "Sprint Retrospective",
-      "email-ids-of-attendees" : "irov@swiggy.com,jolly@swiggy.com,kalashnikov@swiggy.com,lagno@swiggy.com"
-    }
-  ]
 
   if (meetingId != undefined){
     var result = meetings.filter(meeting => meeting['meeting-id'] == meetingId)
     if(result.length != 0){
       res.send(result[0])
     } else {
-      res.status(400).send({"message":"Meeting id doesn’t exist"});
+      res.status(400).send({"message" : "Meeting id doesn’t exist"});
     }
   } else { 
     res.send(meetings)
   }
-})
+});
+
+
+router.delete("/:userId/meetings/:meetingId", (req, res, next) => {
+  const {userId, meetingId} = req.params
+  console.log(userId, meetingId)
+
+  var result = meetings.filter(meeting => meeting['meeting-id'] == meetingId)
+  var person = (result.length == 0) ? null : result[0]['email-ids-of-attendees'].match(userId)
+  if(result.length != 0 && person != null){
+    res.send({"message" : "You are dropped off from the meeting"})
+  } else {
+    res.send({"message" : "Meeting id doesn’t exist"})
+  }
+});
 
 module.exports = router;

3ec7ea9457239cbdbc342639ae627293756643ae
aky91 <aakash28399@gmail.com>
2022-01-07 22:00:27 +0530

    Improved landing page GET /

--- meeting-application/routes/index.js
+++ meeting-application/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const response = await axios.get('https://catfact.ninja/fact');
+  res.render('index', { 
+    title: 'Meeting Application Project', 
+    cat_fact: `${response.data.fact}`
+  });
 });
+
 
 module.exports = router;

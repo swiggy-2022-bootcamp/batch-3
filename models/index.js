18afa3ed0ea8e02e5124f55e9da503a113af6f5a
sachin <sachinsom507@gmail.com>
2022-01-07 19:13:09 +0530

    Refactored: Removed Unnecessary Comments.

--- models/index.js
+++ models/index.js
@@ -1,22 +1,21 @@
-// /* Defining Association Between Models */
-// /*
+/**
+*   Defining Association Between Models
+*
+*   1. One to Many Relationship b/w User ( parent ) and Question ( child ).
+*   2. One to Many Relationship b/w Question ( parent ) and Answer ( child ).
+*   3. One to Many Relationship b/w User ( parent ) and Answer ( child ).
+*/
 
-// 1. One to Many Relationship b/w User ( parent ) and Question ( child ).
-// 2. One to Many Relationship b/w Question ( parent ) and Answer ( child ).
 
-// */
-
-/* Import Models */
 const { User } = require('./user.model');
 const { Question } = require('./question.model');
 const { Answer } = require('./answer.model');
 
 (async () => {
-
     await User.hasMany(Question);
     await Question.belongsTo(User);
 
-    await Question.hasMany(Answer);
+    await Question.hasMany(Answer, { foreignKey: 'question_id'});
     await Answer.belongsTo(Question);
 
     await User.hasMany(Answer);

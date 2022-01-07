bc0be557eaaa256cb39890c93c64bdff7c0a4f98
aky91 <aakash28399@gmail.com>
2022-01-07 22:01:33 +0530

    Added basic POST route to register user

--- meeting-application/routes/users.js
+++ meeting-application/routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST users route to register user*/
+router.post('/', (req, res, next) => {
+
+  var {userid,password} = req.body
+  var registrationName = req.body["registration-name"]
+
+  console.log(registrationName, userid, password)
+  
+  res.status(201).send({
+    'message' : 'User Registered Successfully',
+    'registration-name' : `${registrationName}`
+  })
+});
+
 module.exports = router;

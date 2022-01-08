61c46f17460896ec5a5d312805e5b8e38bee3997
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-01-09 00:54:50 +0530

    Add user login service

---
+++ stackunderflow_backend/src/services/login/userloginservice.ts
@@ -0,0 +1,43 @@
+import { findUserByUsername } from "../../repositories/user";
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
+
+interface UserLoginParameters {
+    username :string;
+    password :string;
+}
+
+export const UserLoginService = async ({username, password} :UserLoginParameters): Promise<string> => {
+    if (!(username && password)) {
+        throw new Error("All parameters needed!");
+    }
+
+    // Check if user exists in the database
+    const user = await findUserByUsername(username);
+    if (user === undefined) {
+        // TODO: set the error code to 409
+        throw new Error("User does not exist. Please SignUP!");
+    }
+    
+    // Check if the password is correct
+    if (await bcrypt.compare(password, user.password) === false) {
+        throw new Error("Password incorrect!");
+    }
+
+    // Create jwt token
+    const token: string = jwt.sign(
+        {id: username},
+        process.env.TOKEN_SECRET,
+        {
+            expiresIn: '2h',
+        }
+    );
+    
+    console.log("Token type: ", typeof(token));
+    return token;
+}

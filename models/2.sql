30c3e481e37d28085ac05c9e66855cae63278cf2
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-01-08 19:11:44 +0530

    Remove UNIQUE contraint from columns

--- models/2.sql
+++ models/2.sql
@@ -3,6 +3,6 @@ create table questions
     pk serial unique not null,
     title text not null,
     body text not null,
-    user_pk int unique not null,
+    user_pk int not null,
     foreign key (user_pk) references users (pk)
 );
--- models/3.sql
+++ models/3.sql
@@ -2,8 +2,8 @@ create table answers
 (
     pk serial unique not null,
     answer text not null,
-    user_pk int unique not null,
-    question_pk int unique not null,
+    user_pk int not null,
+    question_pk int not null,
     foreign key (user_pk) references users (pk),
     foreign key (question_pk) references questions (pk),
     unique (user_pk, question_pk)

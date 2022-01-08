506f95df795ac0c7fcdc9b7560cdbc4719ec7427
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-01-09 00:56:19 +0530

    Add register and login user routes

--- stackunderflow_backend/src/routes/index.router.ts
+++ stackunderflow_backend/src/routes/index.router.ts
@@ -2,6 +2,8 @@ import express from "express";
 import UserRouter from "./user.router";
 import QuestionRouter from "./question.router";
 import HealthcheckController from "../controllers/healthcheck.controller";
+import RegisterController from "../controllers/registeruser.controller";
+import LoginController from "../controllers/login.controller";
 
 const router = express.Router();
 
@@ -9,6 +11,16 @@ router.get("/healthcheck", async (_req, res) => {
   const controller = new HealthcheckController();
   const response = await controller.getMessage();
   return res.send(response);
+});
+
+router.post("/register", async (req, res) => {
+  const controller = new RegisterController();
+  controller.registerUser(req, res);
+});
+
+router.post("/login", async (req, res) => {
+  const controller = new LoginController();
+  controller.loginUser(req, res);
 });
 
 router.use("/users", UserRouter);

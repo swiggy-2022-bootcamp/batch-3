15f2ca26bd8e9feb47750cf3f59e1acdb7958f86
Soham Tupe <tupesoham@gmail.com>
2022-01-09 12:18:55 +0530

    Mongoose DB connection

--- Final Project/app.js
+++ Final Project/app.js
@@ -3,15 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose'); //Require mongoose ODM for mongoDB interaction
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var DB_URL = "mongodb+srv://food_app_super_user:YummyFoods@cluster0.h6s8s.mongodb.net/Food_App_DB?retryWrites=true&w=majority" //Link to MongoDB Atlas instance
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// Connect to database
+mongoose.connect(DB_URL, () => {
+  console.log("DB Connected Successfully!");
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
